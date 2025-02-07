ac59701ec7c524a87d278fc73059787d1988cb7b
Jacob Hambly <jhambly@uoguelph.ca>
2025-02-07 10:20:33 -0500

    Finishing chapter 5

--- chapter5.R
+++ chapter5.R
@@ -372,3 +372,67 @@ drawdag(MElist)
 data("Howell1")
 d <- Howell1
 str(d)
+
+##5.46 - showing more uncertainty in males vs females
+mu_female <- rnorm(1e4, 178, 20)
+mu_male <- rnorm(1e4, 178, 20) + rnorm(1e4, 0, 10)
+precis(data.frame(mu_female, mu_male))
+
+##5.47
+d$sex <- ifelse(d$male==1, 2, 1)
+str(d$sex)
+
+##5.48
+m5.8 <- quap(
+  alist(
+    height ~ dnorm(mu, sigma),
+    mu <- a[sex],
+    a[sex] ~ dnorm(178, 20),
+    sigma ~ dunif(0,50)
+  ), data = d
+)
+precis(m5.8, depth = 2)
+
+##5.49 - difference in expected heights between males and females
+post <- extract.samples(m5.8)
+post$diff_fm <- post$a[,1] - post$a[,2]
+precis(post, depth = 2)
+
+##5.50
+data("milk")
+d <- milk
+levels(d$clade)
+
+##5.51
+d$clade_id <- as.integer(d$clade)
+
+##5.52
+d$K <- standardize(d$kcal.per.g)
+m5.9 <- quap(
+  alist(
+    K ~ dnorm(mu, sigma),
+    mu <- a[clade_id],
+    a[clade_id] ~ dnorm(0,0.5),
+    sigma ~ dexp(1)
+  ), data = d
+)
+precis(m5.9, depth = 2)
+labels <- paste("a[", 1:4, "]:", levels(d$clade), sep="")
+plot(precis(m5.9, depth=2, pars = "a"), labels = labels, xlab = "expected kcal (std)")
+
+##5.53
+set.seed(63)
+d$house <- sample(rep(1:4, each=8), size=nrow(d))
+
+##5.54
+m5.10 <- quap(
+  alist(
+    K ~ dnorm(mu, sigma),
+    mu <- a[clade_id] + h[house],
+    a[clade_id] ~ dnorm(0,0.5),
+    h[house] ~ dnorm(0,0.5),
+    sigma ~ dexp(1)
+  ), data = d
+)
+
+precis(m5.10, depth=2)
